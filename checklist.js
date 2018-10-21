9398a7f0d7fad19b109f42c936f0f5776b8d6678
Guilherme Prado <guiherme.nunes@tecnospeed.com.br>
2018-10-21 00:33:29 -0300

    checklist

---
+++ checklist.js
@@ -0,0 +1,17 @@
+module.exports = [
+  {
+    'type': 'storm',
+    'title': 'Storm Comming',
+    'checklist': [
+      'Stay inside during thunderstorms',
+      'Have a safety kit (flashlights, batteries, first aid)',
+      'Keep an eye on local weather developments, and act appropriately, according to the information you receive on the conditions.',
+      'Close curtains and blinds, try to stay away from windows.',
+      'If caught outside find safe shelter away from trees, power lines, storm water drains and streams.',
+      'Unplug electrical appliances and avoid using landline telephones if there is lightning.',
+      'Park your vehicle undercover and away from trees.',
+      'If you are driving during a storm, slow down and turn headlights on.',
+      'Stay away from metal objects and water.'
+    ]
+  }
+]
